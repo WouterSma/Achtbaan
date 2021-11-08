3866cfcd7d69406b77639b4ac7a097914b04e1d4
Wouter Smaardijk <287574@edu.rocwb.nl>
2021-11-08 11:35:02 +0100

    #2 Foto header toegevoegd

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+        <div class="topnav">
+            <img src="img/logo.jfif">
+            
+            <a class="active" href="#home">Home</a>
+            <a href="#item2">Item2</a>
+            <a href="#item3">Item3</a>
+            <a href="#item4">Item4</a>
+          </div>
+      </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php require_once 'head.php'; ?>
+    <title>Super Coaster</title>  
+  </head>
+  <body>
+  <?php require_once 'header.php'; ?>
+    
+    
+      
+  </body>
+</html>
